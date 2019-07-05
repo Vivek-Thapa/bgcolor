59b5ff38ffb3b827540977a0d838588c89241029
vivek-thapa <vivekthapa261993@gmail.com>
2019-07-05 09:10:12 +0530

    background_changing_demo

---
+++ Desktop/bgcolor-change-demo/main.js
@@ -0,0 +1,8 @@
+ // function to change the web page back ground color
+ function changeBodyBg(color){
+    document.body.style.background = color;
+}
+// function to change the heading background color
+function changeHeadingBg(color){
+    document.getElementById('heading').style.background = color;
+}
